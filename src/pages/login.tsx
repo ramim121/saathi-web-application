37297addf2604f893860cd6119fc539fb1563d69
Samdani Anik <samdanianik49@gmail.com>
2024-05-13 21:35:19 +0600

    Login page in progress

---
+++ src/pages/login.tsx
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+
+const LoginPage = () => {
+	const [email, setEmail] = useState<string>('');
+	const [password, setPassword] = useState<string>('');
+
+	const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setEmail(e.target.value);
+	};
+
+	const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setPassword(e.target.value);
+	};
+
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const formData = {
+			email: email,
+			password: password
+		};
+
+		try {
+			const res = await fetch(process.env.API_URL + 'api/login', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(formData)
+			});
+
+
+			const data = await res.json();
+
+			if (data.success == true) {
+				console.log('Login successful');
+			}
+
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	return (
+		<div className='loginContainer'>
+			<form className='loginForm' onSubmit={handleSubmit}>
+				<h2>Login</h2>
+				<div className='loginFormGroup'>
+					<label htmlFor="email">Email:</label>
+					<input type="email" id="email" value={email} onChange={handleEmailChange} />
+				</div>
+				<div className='loginFormGroup'>
+					<label htmlFor="password">Password:</label>
+					<input type="password" id="password" value={password} onChange={handlePasswordChange} />
+				</div>
+				<button type="submit">Login</button>
+			</form>
+		</div>
+	);
+};
+
+export default LoginPage;
