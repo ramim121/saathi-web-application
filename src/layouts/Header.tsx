7c84cf42df72cf7139c6357350e5a112ca525408
Samdani Anik <samdanianik49@gmail.com>
2024-06-11 07:50:34 +0600

    Booking list in progress

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -75,6 +75,9 @@ function Header() {
                                 <NavDropdown title="Investments" id="basic-nav-dropdown">
                                     <NavDropdown.Item href="/setup/investment">Setup</NavDropdown.Item>
                                 </NavDropdown>
+                                <NavDropdown title="Booking" id="basic-nav-dropdown">
+                                    <NavDropdown.Item href="/booking/list">List</NavDropdown.Item>
+                                </NavDropdown>
                                 <NavDropdown title="Blogs" id="basic-nav-dropdown">
                                     <NavDropdown.Item href="/blogs/create">Create</NavDropdown.Item>
                                     <NavDropdown.Item href="/blogs/list">List</NavDropdown.Item>
