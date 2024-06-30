88ae2cabbcfe31ebc974ee37c0093f53e1c67e39
Adnan Nuruddin <get2adnan@windowslive.com>
2024-06-30 17:27:53 +0600

    User info update should work

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -3,7 +3,22 @@ import jwt from 'jsonwebtoken';
 import JWTPayload from '@/types/JWTPayload';
 import { JWT_SECRET } from '@/config/constants';
 import { User, Project, ProjectInvestor, ProjectPartner } from '@/models/__associations';
+import Joi from 'joi';
 
+const schema = Joi.object({
+    fullName: Joi.string().required().messages({
+        "any.required": "Name is required",
+        "string.empty": "Name can not be empty",
+    }),
+    email: Joi.string().email().required().messages({
+        "any.required": "email is required",
+        "string.email": "Invalid email",
+    }),
+    dateOfBirth: Joi.date().required().messages({
+        "any.required": "Date of birth is required",
+        "date.base": "Date of birth must be a date",
+    }),
+}).unknown();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
@@ -21,8 +36,21 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             ],
         })
 
-    if (!user) { res.status(404).json({ success: false, message: 'User not found' }); return; }
-    if (req.method == 'GET') { res.status(200).json({ success: true, user }); return; }
+    if (!user) { res.status(404).json({ message: 'User not found' }); return; }
+    if (req.method == 'GET') { res.status(200).json({ user }); return; }
+    if (req.method == 'PUT') {
+        const { error } = schema.validate(req.body, { abortEarly: false });
+        if (error) {
+            let errorMessage: string[] = [];
+            error.details.forEach((e) => {
+                errorMessage.push(e.message);
+            });
+            return res.status(400).json({ success: false, message: errorMessage });
+        }
+
+        let updatedUser = await User.update(req.body, { where: { idUsers: userInfo!.idUsers } });
+        res.status(200).json({ updatedUser });
+    }
 
 
 }
