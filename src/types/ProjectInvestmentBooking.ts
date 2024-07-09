99065c22d99666f5a3b7933d72b0efcda9121294
Adnan Nuruddin <get2adnan@windowslive.com>
2024-07-09 10:50:28 +0600

    Data added

--- src/types/ProjectInvestmentBooking.ts
+++ src/types/ProjectInvestmentBooking.ts
@@ -1,6 +1,6 @@
 import { Optional } from 'sequelize';
 
-import UserBank  from './UserBank';
+import UserBank from './UserBank';
 import User from './User';
 import ProjectInvestor from './ProjectInvestor';
 
@@ -8,6 +8,7 @@ export default interface ProjectInvestmentBooking {
     idProjectInvestmentBookings: number;
     idUsers: number;
     paymentMethod: 'bank' | 'cash' | 'card' | 'mobile';
+    paymentConfirmationStatus: 'pending' | 'confirmed' | 'denied';
     bookingId?: string;
     paymentAmount?: number;
     transactionId?: string;
