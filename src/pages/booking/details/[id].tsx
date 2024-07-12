1c8fafd1ad3a8964fda004e2a304ef316d4cd70c
AdnanNuruddin <get2adnan@windowslive.com>
2024-07-12 15:10:19 +0600

    Update [id].tsx

---
+++ src/pages/booking/details/[id].tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BlankComponent = () => {
+  return (
+    <div>
+      {/* Add your component code here */}
+    </div>
+  );
+};
+
+export default BlankComponent;
