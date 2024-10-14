fd31bffb6ebf158b4f8c52e10d40d3bd478b64bc
Adnan Nuruddin <get2adnan@windowslive.com>
2024-10-14 20:15:58 +0600

    db constant changed

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,12 +3,13 @@ export const API_URL = process.env.API_URL ? process.env.API_URL : 'https://digi
 
 export const JWT_SECRET = process.env.JWT_SECRET ? process.env.JWT_SECRET : '778^%009jjnuybbuhuh009*^&^';
 
-export const BULK_SMS_API_KEY = process.env.BULK_SMS_API_KEY ? process.env.BULK_SMS_API_KEY : '4p9RrLDJg0Jm4hx1YfDE'
-export const BULK_SMS_SENDER_ID = process.env.BULK_SMS_SENDER_ID ? process.env.BULK_SMS_SENDER_ID : '8809617618599'
+export const BULK_SMS_API_KEY = process.env.BULK_SMS_API_KEY ? process.env.BULK_SMS_API_KEY : '4p9RrLDJg0Jm4hx1YfDE';
+export const BULK_SMS_SENDER_ID = process.env.BULK_SMS_SENDER_ID ? process.env.BULK_SMS_SENDER_ID : '8809617618599';
 export const OTP_EXPIRY = process.env.OTP_EXPIRY ? process.env.OTP_EXPIRY : 5 * 60 * 1000 // 5 minutes
 
-export const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME ? process.env.S3_BUCKET_NAME : 'saathi-files'
-export const S3_BUCKET_REGION = process.env.S3_BUCKET_REGION ? process.env.S3_BUCKET_REGION : 'ap-southeast-1'
-export const S3_BUCKET_ACCESS_KEY = process.env.S3_BUCKET_ACCESS_KEY ? process.env.S3_BUCKET_ACCESS_KEY : 'AKIA4MTWJKAASBQ5ENWP'
-export const S3_BUCKET_SECRET_KEY = process.env.S3_BUCKET_SECRET_KEY ? process.env.S3_BUCKET_SECRET_KEY : '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'
-export const S3_URL = process.env.S3_URL ? process.env.S3_URL : 'https://saathi-files.s3.ap-southeast-1.amazonaws.com/'
+export const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME ? process.env.S3_BUCKET_NAME : 'saathi-files';
+export const S3_BUCKET_REGION = process.env.S3_BUCKET_REGION ? process.env.S3_BUCKET_REGION : 'ap-southeast-1';
+export const S3_BUCKET_ACCESS_KEY = process.env.S3_BUCKET_ACCESS_KEY ? process.env.S3_BUCKET_ACCESS_KEY : 'AKIA4MTWJKAASBQ5ENWP';
+export const S3_BUCKET_SECRET_KEY = process.env.S3_BUCKET_SECRET_KEY ? process.env.S3_BUCKET_SECRET_KEY : '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';
+export const S3_URL = process.env.S3_URL ? process.env.S3_URL : 'https://saathi-files.s3.ap-southeast-1.amazonaws.com/';
+export const DB_NAME = process.env.DB_NAME ? process.env.DB_NAME : 'saathi_db';
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,14 +1,16 @@
 import { Sequelize } from 'sequelize';
+import { DB_NAME } from './constants';
+
 
 // Option 1: Passing a connection URI
 const sequelize = new Sequelize({
-    dialect:'mysql',
+    dialect: 'mysql',
     dialectModule: require('mysql2'),
     host: 'saathi-db.cla6si4uaanu.ap-southeast-1.rds.amazonaws.com',
     port: 3306,
     username: 'saathi_admin',
     password: 'w607kTcCngWiq8U',
-    database: 'saathi_db'
+    database: DB_NAME
 });// Example for postgres
 
 export default sequelize;
