92701b2efab2a908e6e521f791c88f17c30cf795
Adnan Nuruddin <get2adnan@windowslive.com>
2024-10-20 19:21:32 +0600

    Profile image for google fixed

--- src/pages/api/auth/google.ts
+++ src/pages/api/auth/google.ts
@@ -1,9 +1,20 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import Cors from 'micro-cors';
 import { OAuth2Client } from 'google-auth-library';
-import { GOOGLE_CLIENT_ID, JWT_SECRET } from '@/config/constants';
+import { GOOGLE_CLIENT_ID, JWT_SECRET, S3_BUCKET_ACCESS_KEY, S3_BUCKET_REGION, S3_BUCKET_SECRET_KEY, S3_BUCKET_NAME } from '@/config/constants';
 import { Project, ProjectInvestor, ProjectPartner, User } from '@/models/__associations';
 import jwt from 'jsonwebtoken';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { Upload } from '@aws-sdk/lib-storage';
+import { generateHash } from '@/utils/GenerateHash';
+import axios from 'axios';
+const s3Client = new S3Client({
+    region: S3_BUCKET_REGION,
+    credentials: {
+        accessKeyId: S3_BUCKET_ACCESS_KEY,
+        secretAccessKey: S3_BUCKET_SECRET_KEY
+    }
+});
 
 // Initialize the Google OAuth2 client
 const client = new OAuth2Client(GOOGLE_CLIENT_ID);
@@ -59,10 +70,12 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
         });
 
         if (!user) {
+            const fileName = await uploadProfilePictureToS3(picture!, googleId!);
+
             user = new User();
             user.email = email!;
             user.fullName = name!;
-            user.profileImage = picture!;
+            user.profileImage = fileName;
             user.googleId = googleId!;
             user.emailVerified = 'yes';
             user.googleLogin = 'yes';
@@ -73,11 +86,35 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
             expiresIn: '30d'
         });
 
+
+
         return res.status(200).json({ success: true, token, user });
 
     } catch (error) {
         console.error('Error during Google Sign-In:', error);
         res.status(500).json({ message: 'Authentication failed', error: (error as Error).message });
+    }
+}
+
+async function uploadProfilePictureToS3(imageUrl: string, userId: string): Promise<string> {
+    try {
+        const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+        const buffer = Buffer.from(response.data, 'binary');
+
+        const fileName = generateHash(Date.now() + userId.toString() + imageUrl) + '.jpeg';
+        const key = `profile/`+fileName;
+
+        await s3Client.send(new PutObjectCommand({
+            Bucket: S3_BUCKET_NAME,
+            ACL: 'public-read',
+            Key: key,
+            Body: buffer,
+            ContentType: `image/jpeg`,
+        }));
+
+        return fileName;
+    } catch (error) {
+        throw error;
     }
 }
 
