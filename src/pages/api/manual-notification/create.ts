a15233652ebf971652ec1f6733d1f6e2f0bb513e
SamdaniAnik <samdanianik49@gmail.com>
2024-11-06 15:13:26 +0600

    Manual notification in progress

---
+++ src/pages/api/manual-notification/create.ts
@@ -0,0 +1,168 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { ManualNotification } from '@/models/__associations';
+import Joi from 'joi';
+import sequelize from '@/config/db';
+import { S3_BUCKET_ACCESS_KEY, S3_BUCKET_SECRET_KEY, S3_BUCKET_REGION, S3_BUCKET_NAME } from '@/config/constants';
+import { generateHash } from '@/utils/GenerateHash';
+import * as formidable from 'formidable';
+import fs from 'fs';
+import _ from 'await-to-js';
+import { S3Client } from '@aws-sdk/client-s3';
+import { Upload } from '@aws-sdk/lib-storage';
+import Cors from 'micro-cors';
+
+const s3Client = new S3Client({
+    region: S3_BUCKET_REGION,
+    credentials: {
+        accessKeyId: S3_BUCKET_ACCESS_KEY,
+        secretAccessKey: S3_BUCKET_SECRET_KEY
+    }
+});
+const cors = Cors({
+    origin: '*',
+    allowMethods: ['GET', 'POST', 'OPTIONS', 'PUT'],
+    allowHeaders: ['X-Requested-With', 'Authorization', 'Content-Type'],
+});
+
+const schema = Joi.object({
+    sendViaSms: Joi.string().required().valid('yes', 'no').messages({
+        "any.required": "Send Via Sms is required",
+        "string.empty": "Send Via Sms can not be empty",
+        "any.only": "Invalid Send Via Sms",
+    }),
+    smsBody: Joi.when('sendViaSms', {
+        is: 'yes',
+        then: Joi.string().required().messages({
+            "any.required": "Sms Body is required when Send Via Sms is 'yes'",
+            "string.empty": "Sms Body cannot be empty",
+        }),
+        otherwise: Joi.optional(),
+    }),
+    sendViaEmail: Joi.string().required().valid('yes', 'no').messages({
+        "any.required": "Send Via Email is required",
+        "string.empty": "Send Via Email can not be empty",
+        "any.only": "Invalid Send Via Email",
+    }),
+    emailSubject: Joi.when('sendViaEmail', {
+        is: 'yes',
+        then: Joi.string().required().messages({
+            "any.required": "Email Subject is required when Send Via Email is 'yes'",
+            "string.empty": "Email Subject cannot be empty",
+        }),
+        otherwise: Joi.optional(),
+    }),
+    emailBody: Joi.when('sendViaEmail', {
+        is: 'yes',
+        then: Joi.string().required().messages({
+            "any.required": "Email Body is required when Send Via Email is 'yes'",
+            "string.empty": "Email Body cannot be empty",
+        }),
+        otherwise: Joi.optional(),
+    }),
+    sendViaPush: Joi.string().required().valid('yes', 'no').messages({
+        "any.required": "Send Via Push is required",
+        "string.empty": "Send Via Push can not be empty",
+        "any.only": "Invalid Send Via Push",
+    }),
+    pushNotificationBody: Joi.when('sendViaPush', {
+        is: 'yes',
+        then: Joi.string().required().messages({
+            "any.required": "Push Notification Body is required when Send Via Push is 'yes'",
+            "string.empty": "Push Notification Body cannot be empty",
+        }),
+        otherwise: Joi.optional(),
+    }),
+    pushNotificationTitle: Joi.when('sendViaPush', {
+        is: 'yes',
+        then: Joi.string().required().messages({
+            "any.required": "Push Notification Title is required when Send Via Push is 'yes'",
+            "string.empty": "Push Notification Title cannot be empty",
+        }),
+        otherwise: Joi.optional(),
+    })
+}).unknown();
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'OPTIONS') { return res.status(200).end(); }
+    if (req.method === 'POST') {
+        const form = new formidable.IncomingForm();
+        form.parse(req, async (err, fields, files) => {
+            if (err) {
+                return res.status(500).json({ success: false, message: err.message });
+            }
+
+            const pushNotificationImage = files['pushNotificationImage'] ? files['pushNotificationImage'][0] as formidable.File : null;
+            const data = {
+                sendViaSms: fields.sendViaSms ? fields.sendViaSms[0] : null,
+                smsBody: fields.smsBody ? fields.smsBody[0] : null,
+                sendViaEmail: fields.sendViaEmail ? fields.sendViaEmail[0] : null,
+                emailSubject: fields.emailSubject ? fields.emailSubject[0] : null,
+                emailBody: fields.emailBody ? fields.emailBody[0] : null,
+                sendViaPush: fields.sendViaPush ? fields.sendViaPush[0] : null,
+                pushNotificationBody: fields.pushNotificationBody ? fields.pushNotificationBody[0] : null,
+                pushNotificationTitle: fields.pushNotificationTitle ? fields.pushNotificationTitle[0] : null
+            };
+
+            const options = {
+                abortEarly: false,
+            };
+            const { error } = schema.validate(data, options);
+
+            if (error) {
+                let errorMessage: string[] = [];
+
+                error.details.forEach((e) => {
+                    errorMessage.push(e.message);
+                });
+                return res.status(400).json({ success: false, message: errorMessage.join(". <br>") });
+            }
+            const params = {
+                Bucket: S3_BUCKET_NAME,
+                ACL: 'public-read'
+            };
+
+            const transaction = await sequelize.transaction();
+            try {
+                let pushNotificationImageName = null;
+                if (pushNotificationImage !== null) {
+                    pushNotificationImageName = generateHash(Date.now() + pushNotificationImage.originalFilename!.toString()) + '.' + pushNotificationImage.originalFilename!.split('.').pop();
+                    const upload = new Upload({
+                        client: s3Client,
+                        params: { ...params, ContentType: pushNotificationImage.mimetype!, Body: fs.createReadStream(pushNotificationImage.filepath), Key: 'push-notification/' + pushNotificationImageName } as any
+                    });
+
+                    upload.on('httpUploadProgress', (progress: any) => {
+                        console.log(`Uploaded ${progress.loaded} of ${progress.total} bytes`);
+                    });
+                    let [err1] = await _(upload.done());
+                    if (err1) {
+                        await transaction.rollback();
+                        return res.status(500).json({ success: false, message: "Push notification image upload error. " + err1.message });
+                    }
+                }
+
+                const notification = await ManualNotification.create({
+                    sendViaSms: data.sendViaSms,
+                    smsBody: data.sendViaSms === 'yes' ? data.smsBody : null,
+                    sendViaEmail: data.sendViaEmail,
+                    emailSubject: data.sendViaEmail === 'yes' ? data.emailSubject : null,
+                    emailBody: data.sendViaEmail === 'yes' ? data.emailBody : null,
+                    sendViaPush: data.sendViaPush,
+                    pushNotificationBody: data.sendViaPush === 'yes' ? data.pushNotificationBody : null,
+                    pushNotificationTitle: data.sendViaPush === 'yes' ? data.pushNotificationTitle : null,
+                    pushNotificationImage: (data.sendViaPush === 'yes' && pushNotificationImage !== null) ? pushNotificationImageName : null
+                }, { transaction });
+
+                await transaction.commit();
+                return res.status(200).json({ success: true, message: 'Manual Notification created successfully', data: notification });
+            } catch (err) {
+                await transaction.rollback();
+                return res.status(500).json({ success: false, message: (err as Error).message });
+            }
+        });
+    } else {
+        res.status(405).json({ success: false, message: 'Method not allowed' });
+    }
+}
+
+export default cors(handler as any);
--- src/pages/manualNotification/create.tsx
+++ src/pages/manualNotification/create.tsx
@@ -1,11 +1,245 @@
+import React, { useState, useRef, useContext } from 'react';
 import MainLayout from '@/layouts/MainLayout';
+import { Container, Row, Col, Form, Button, Spinner } from 'react-bootstrap';
+import { Editor } from '@tinymce/tinymce-react';
+import { API_URL } from '@/config/constants';
+import Swal from 'sweetalert2';
+import { getCookie } from '@/utils/GetCookie';
+import { AppContext } from '@/context/AppContext';
+
+interface FormDataType {
+    createdBy?: number;
+    sendViaSms: string;
+    smsBody: string;
+    sendViaEmail: string;
+    emailSubject: string;
+    emailBody: string;
+    sendViaPush: string;
+    pushNotificationTitle: string;
+    pushNotificationBody: string;
+    pushNotificationImage: File | null;
+}
 
 function ManualNotification() {
+    const { token, currentUser } = useContext(AppContext);
+    const [formData, setFormData] = useState<FormDataType>({
+        sendViaSms: 'no',
+        smsBody: '',
+        sendViaEmail: 'no',
+        emailSubject: '',
+        emailBody: '',
+        sendViaPush: 'no',
+        pushNotificationTitle: '',
+        pushNotificationBody: '',
+        pushNotificationImage: null
+    });
+    const editorRef = useRef<any>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const handleTextChange = (e: any) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    }
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setLoading(true);
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You want to create this manual notification!",
+            icon: 'warning',
+            showCancelButton: true,
+            cancelButtonText: 'No',
+            confirmButtonText: 'Yes'
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                try {
+                    formData.createdBy = currentUser?.idUsers || undefined;
+                    const newFormData = new FormData();
+                    newFormData.append('sendViaSms', formData.sendViaSms);
+                    newFormData.append('smsBody', formData.smsBody);
+                    newFormData.append('sendViaEmail', formData.sendViaEmail);
+                    newFormData.append('emailSubject', formData.emailSubject);
+
+                    if (editorRef.current !== null) {
+                        newFormData.append('emailBody', editorRef.current.getContent());
+                    } else {
+                        newFormData.append('emailBody', formData.emailBody);
+                    }
+
+                    newFormData.append('sendViaPush', formData.sendViaPush);
+                    newFormData.append('pushNotificationTitle', formData.pushNotificationTitle);
+                    newFormData.append('pushNotificationBody', formData.pushNotificationBody);
+
+                    if (formData.pushNotificationImage) {
+                        newFormData.append('pushNotificationImage', formData.pushNotificationImage);
+                    }
+
+                    const res = await fetch(`${API_URL}api/manual-notification/create`, {
+                        method: 'POST',
+                        headers: { 'Authorization': 'Bearer ' + getCookie('saathi-token') },
+                        body: newFormData,
+                    });
+
+                    if (res.ok) {
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Success',
+                            text: 'Manual notification created successfully!',
+                        });
+                    } else {
+                        const errorData = await res.json();
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Error',
+                            html: errorData.message,
+                        });
+                    }
+                } catch (err) {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Error',
+                        text: 'Something went wrong!',
+                    });
+                } finally {
+                    setLoading(false);
+                }
+            } else {
+                setLoading(false);
+            }
+        });
+    };
 
     return (
-        <div>
-            ManualNotification
-        </div>
+        <>
+            <Container>
+                <Row className="justify-content-center">
+                    <Col md={6}>
+                        <h4 className="text-start">Manual Notification Create</h4>
+                        <hr />
+                        <Form onSubmit={handleSubmit}>
+                            <Form.Group as={Row}>
+                                <Form.Label column sm='4' className='mb-3'>Send Via Sms<span className='text-danger'>*</span></Form.Label>
+                                <Col sm='8'>
+                                    <Form.Select name='sendViaSms' onChange={handleTextChange} value={formData.sendViaSms}>
+                                        <option>Select Option</option>
+                                        <option value="yes">Yes</option>
+                                        <option value="no">No</option>
+                                    </Form.Select>
+                                </Col>
+                            </Form.Group>
+                            {formData.sendViaSms === 'yes' &&
+                                <Form.Group as={Row}>
+                                    <Form.Label column sm='4' className='mb-3'>Sms Body<span className='text-danger'>*</span></Form.Label>
+                                    <Col sm='8'>
+                                        <Form.Control as='textarea' name='smsBody' value={formData.smsBody} onChange={handleTextChange} />
+                                    </Col>
+                                </Form.Group>
+                            }
+                            <Form.Group as={Row} className='mt-2'>
+                                <Form.Label column sm='4' className='mb-3'>Send Via Email<span className='text-danger'>*</span></Form.Label>
+                                <Col sm='8'>
+                                    <Form.Select name='sendViaEmail' onChange={handleTextChange} value={formData.sendViaEmail}>
+                                        <option>Select Option</option>
+                                        <option value="yes">Yes</option>
+                                        <option value="no">No</option>
+                                    </Form.Select>
+                                </Col>
+                            </Form.Group>
+                            {formData.sendViaEmail === 'yes' &&
+                                <>
+                                    <Form.Group as={Row} className='mb-3'>
+                                        <Form.Label column sm='4' className='mb-3'>Email Subject<span className='text-danger'>*</span></Form.Label>
+                                        <Col sm='8'>
+                                            <Form.Control type='text' name='emailSubject' value={formData.emailSubject} onChange={handleTextChange} />
+                                        </Col>
+                                    </Form.Group>
+                                    <Form.Group as={Row} className='mb-3'>
+                                        <Form.Label column sm='4' className='mb-3'>Email Body<span className='text-danger'>*</span></Form.Label>
+                                        <Col sm='8'>
+                                            <Editor
+                                                apiKey="abqylwi3epqtdz7e4t0aasmr5f62etpkkrrd9kiuktqf004r"
+                                                onInit={(evt, editor) => editorRef.current = editor}
+                                                id='summary'
+                                                init={{
+                                                    height: 400,
+                                                    plugins: [
+                                                        'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+                                                        'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+                                                        'insertdatetime', 'media', 'table', 'help', 'wordcount'
+                                                    ],
+                                                    toolbar: 'undo redo | blocks | ' +
+                                                        'bold italic backcolor | alignleft aligncenter ' +
+                                                        'alignright alignjustify | bullist numlist outdent indent | ' +
+                                                        'removeformat | help',
+                                                    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }'
+                                                }}
+                                            />
+                                        </Col>
+                                    </Form.Group>
+                                </>
+                            }
+                            <Form.Group as={Row}>
+                                <Form.Label column sm='4' className='mb-3'>Send Via Push<span className='text-danger'>*</span></Form.Label>
+                                <Col sm='8'>
+                                    <Form.Select name='sendViaPush' onChange={handleTextChange} value={formData.sendViaPush}>
+                                        <option>Select Option</option>
+                                        <option value="yes">Yes</option>
+                                        <option value="no">No</option>
+                                    </Form.Select>
+                                </Col>
+                            </Form.Group>
+                            {formData.sendViaPush === 'yes' &&
+                                <>
+                                    <Form.Group as={Row} className='mb-3'>
+                                        <Form.Label column sm='4' className='mb-3'>Push Notification Title<span className='text-danger'>*</span></Form.Label>
+                                        <Col sm='8'>
+                                            <Form.Control type='text' name='pushNotificationTitle' value={formData.pushNotificationTitle} onChange={handleTextChange} />
+                                        </Col>
+                                    </Form.Group>
+                                    <Form.Group as={Row} className='mb-3'>
+                                        <Form.Label column sm='4' className='mb-3'>Push Notification Body<span className='text-danger'>*</span></Form.Label>
+                                        <Col sm='8'>
+                                            <Form.Control as='textarea' name='pushNotificationBody' value={formData.pushNotificationBody} onChange={handleTextChange} />
+                                        </Col>
+                                    </Form.Group>
+                                    <Form.Group as={Row} className='mb-3'>
+                                        <Form.Label column sm='4' className='mb-3'>Push Notification Image</Form.Label>
+                                        <Col sm='8'>
+                                            <Form.Control type='file' name='pushNotificationImage' onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                                if (e.target.files && e.target.files[0]) {
+                                                    setFormData({
+                                                        ...formData,
+                                                        pushNotificationImage: e.target.files[0]
+                                                    });
+                                                }
+                                            }} />
+                                        </Col>
+                                    </Form.Group>
+
+                                </>
+                            }
+                            <Row>
+                                <Col sm='4'></Col>
+                                <Col sm='8'>
+                                    <Row className='justify-content-center'>
+                                        <Button className='w-50' variant="primary" type="submit" disabled={loading}>
+                                            {loading && <Spinner as="span" animation="grow" size="sm" role="status" aria-hidden="true" />}
+                                            {loading ? 'Submitting...' : 'Submit'}
+                                        </Button>
+                                    </Row>
+                                </Col>
+                            </Row>
+                        </Form>
+                    </Col>
+                </Row>
+                <pre>{JSON.stringify(formData, null, 2)}</pre>
+            </Container>
+        </>
     )
 }
 
