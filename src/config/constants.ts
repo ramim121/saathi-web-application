c63ea1ffe9359f33426dec0668931f929935e5fe
AdnanNuruddin <get2adnan@windowslive.com>
2024-11-17 10:10:02 +0600

    Update constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = process.env.API_URL ? process.env.API_URL : 'https://digigramventures.com/';
+export const API_URL = process.env.API_URL ? process.env.API_URL : 'https://api.digigramventures.com/';
 // export const API_URL = 'http://localhost:3000/';
 
 export const JWT_SECRET = process.env.JWT_SECRET ? process.env.JWT_SECRET : '778^%009jjnuybbuhuh009*^&^';
