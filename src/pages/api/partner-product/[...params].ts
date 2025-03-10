3436548ff58f8834c03e2c3da9fcef4688ffd25a
Adnan Nuruddin <get2adnan@windowslive.com>
2025-03-10 20:06:57 +0600

    Product details for mart added

---
+++ src/pages/api/partner-product/[...params].ts
@@ -0,0 +1,44 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { db } from '@/config/db';
+import { Product, ProductCategory, ProductImage, ProductPacking, ProductPartner, User } from '@/models/__associations';
+import Cors from 'micro-cors';
+
+const cors = Cors({
+    origin: '*',
+    allowMethods: ['GET', 'POST', 'OPTIONS', 'PUT'],
+    allowHeaders: ['X-Requested-With', 'Authorization', 'Content-Type'],
+});
+
+async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    const [idUsers, idProducts] = req.query.params as string[];
+
+    const userData = await User.findOne({
+        where: { idUsers: idUsers }
+    });
+    if (!userData) {
+        res.status(404).json({ success: false, message: 'User not found' });
+        return;
+    }
+
+    const productDetails = await Product.findOne({
+        include: [ProductImage, ProductCategory, {
+            model: ProductPartner,
+            include: [ProductPacking],
+            where: { idUsers: idUsers }
+        }],
+        where: { idProducts: idProducts }
+    });
+
+    if (!productDetails) {
+        res.status(404).json({ success: false, message: 'Product not found' });
+        return;
+    }
+
+
+    res.status(200).send({ product: productDetails, user: userData });
+}
+
+export default cors(handler as any);
