17a20403e784ccadd9c5475490745abb29ab8ca5
Ramim Sakhawat Hossain <ramim121@gmail.com>
2026-03-07 15:25:40 +0600

    Update constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -7,11 +7,11 @@ export const BULK_SMS_API_KEY = process.env.BULK_SMS_API_KEY ? process.env.BULK_
 export const BULK_SMS_SENDER_ID = process.env.BULK_SMS_SENDER_ID ? process.env.BULK_SMS_SENDER_ID : '8809617618599';
 export const OTP_EXPIRY = process.env.OTP_EXPIRY ? process.env.OTP_EXPIRY : 1 * 60 * 1000 // 5 minutes
 
-export const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME ? process.env.S3_BUCKET_NAME : 'saathi-files';
+export const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME ? process.env.S3_BUCKET_NAME : 'saathi-production-2025';
 export const S3_BUCKET_REGION = process.env.S3_BUCKET_REGION ? process.env.S3_BUCKET_REGION : 'ap-southeast-1';
 export const S3_BUCKET_ACCESS_KEY = process.env.S3_BUCKET_ACCESS_KEY ? process.env.S3_BUCKET_ACCESS_KEY : 'AKIA4MTWJKAASBQ5ENWP';
 export const S3_BUCKET_SECRET_KEY = process.env.S3_BUCKET_SECRET_KEY ? process.env.S3_BUCKET_SECRET_KEY : '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';
-export const S3_URL = process.env.S3_URL ? process.env.S3_URL : 'https://saathi-files-new.s3.ap-southeast-1.amazonaws.com/';
+export const S3_URL = process.env.S3_URL ? process.env.S3_URL : 'https://saathi-production-2025.s3.ap-southeast-1.amazonaws.com/';
 
 export const DB_NAME = process.env.DB_NAME ? process.env.DB_NAME : 'saathi_db';
 export const DB_USER = process.env.DB_USER ? process.env.DB_USER : 'saathi_admin';
